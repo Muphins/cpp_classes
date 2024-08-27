fd2afbb83031d0c9cf73cee0ece53e449a9a6bcc
Thibault.G <75988095+Muphins@users.noreply.github.com>
2024-08-27 18:25:13 +0200

    Create 2D_geometry.cpp

---
+++ 2D_geometry.cpp
@@ -0,0 +1,85 @@
+#include <cmath>
+
+
+#define PI 3.14159265359
+#define RAD(a) (a*PI/180)
+class cAngle{
+private:
+	float angle;
+	
+	float cap() {
+	    while(angle > PI) angle -= 2*PI;
+	    while(angle < -PI) angle += 2*PI;
+	    return angle;
+	}
+public:
+    //== Construstors
+	cAngle(const float value) : angle(value) {}
+	cAngle(): angle(0) {}
+	//== Operators
+	operator float() const {
+	    return cAngle(angle).cap();
+	}
+	cAngle& operator= (const float& value) {
+	    this->angle = value;
+	    cap();
+	    return *this; 
+	}
+	cAngle& operator+= (const float& value) {
+	    this->angle += value;
+	    cap();
+	    return *this; 
+	}
+	cAngle& operator-= (const float& value) {
+	    this->angle -= value;
+	    cap();
+	    return *this; 
+	}
+	//== Functions
+	float deg() const {
+	    return angle *180/PI;
+	}
+};
+
+class cVector {
+private:
+    float x, y;
+
+public:
+    //== Constructors
+    cVector(): x(0), y(0) {}
+    cVector(float x_, float y_): x(x_), y(y_) {}
+    cVector(cAngle angle, float mag){
+        x = std::cos(angle)*mag;
+        y = std::sin(angle)*mag;
+    }
+    //== Operators
+    cVector& operator+=(const cVector& rhs){
+        x+=rhs.x;
+        y+=rhs.y;
+        return *this;
+    }
+    cVector& operator-=(const cVector& rhs){
+        x-=rhs.x;
+        y-=rhs.y;
+        return *this;
+    }
+    template <typename Scalar>
+    cVector& operator*=(const Scalar scalar){
+        x*=scalar;
+        y*=scalar;
+        return *this;
+    }
+    //== Functions
+    cAngle angleto(cVector rhs){
+        float dot = this->x * rhs.x + this->y * rhs.y;
+        float det = this->x * rhs.y + this->y * rhs.x;
+        return std::atan2(det, dot);
+    }
+    cAngle angle(){
+        return std::atan2(y,x);
+    }
+    float mag(){
+        return std::sqrt(x*x + y*y);
+    }
+};
